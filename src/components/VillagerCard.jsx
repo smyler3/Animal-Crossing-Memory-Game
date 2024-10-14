c62f2151e3753ea3f311e730749458230c470320
smyler3 <mylertalym01@gmail.com>
2024-10-14 22:35:09 +1100

    feat: make cards buttons and dynamic on hover focus and active

--- src/components/VillagerCard.jsx
+++ src/components/VillagerCard.jsx
@@ -3,14 +3,14 @@ import "../styles/VillagerCard.css";
 // TODO: add full body images on larger screens?
 const VillagerCard = ({ villager }) => {
     return (
-        <div className="villager-card">
+        <button className="villager-card">
             <div className="villager-img">
                 <img src={`${villager.nh_details.icon_url}`} alt={`Icon of ${villager.name}`} />
             </div>
             <div className="villager-title-card" style={{backgroundColor: villager.title_color}}>
                 <p className="villager-title" style={{color: villager.text_color}}>{villager.name}</p>
             </div>
-        </div>
+        </button>
     )
 }
 
