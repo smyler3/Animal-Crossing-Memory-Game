bcbb897c64306e59e665478c98aedaa2c010635a
smyler3 <mylertalym01@gmail.com>
2024-10-16 01:53:10 +1100

    fix: make cards divs to prevent focus state from sticking to last clicked card

--- src/components/VillagerCard.jsx
+++ src/components/VillagerCard.jsx
@@ -3,14 +3,14 @@ import "../styles/VillagerCard.css";
 // TODO: add full body images on larger screens?
 const VillagerCard = ({ villager, handleClick, }) => {
     return (
-        <button className="villager-card" onClick={() => {handleClick(villager.id)}}>
+        <div className="villager-card" onClick={() => {handleClick(villager.id)}}>
             <div className="villager-img">
                 <img src={`${villager.icon_url}`} alt={`Icon of ${villager.name}`} />
             </div>
             <div className="villager-title-card" style={{backgroundColor: villager.title_color}}>
                 <p className="villager-title" style={{color: villager.text_color}}>{villager.name}</p>
             </div>
-        </button>
+        </div>
     )
 };
 
